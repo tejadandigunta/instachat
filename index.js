72bd8846b686846757bea744bd222f6aa716c3b9
Teja Dandigunta <teja.dandigunta@example.com>
2026-05-03 13:03:24 +0530

    Minor update

--- index.js
+++ index.js
@@ -95,4 +95,5 @@ async function processQueue() {
 }
 
 // ===== START SERVER =====
-app.listen(3000, () => console.log("Running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running"));
