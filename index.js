77894b4b838fefa3fd4ea9d099cf70d15a44b264
Teja Dandigunta <teja.dandigunta@example.com>
2026-05-03 14:55:00 +0530

    variable fixes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("OK");
 });
 
 const VERIFY_TOKEN = "myverifytoken";
-const PAGE_ACCESS_TOKEN = "process.env.PAGE_ACCESS_TOKEN";
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // ===== WEBHOOK VERIFY =====
 app.get("/webhook", (req, res) => {
@@ -19,7 +19,7 @@ app.get("/webhook", (req, res) => {
     const token = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
 
-    if (mode === "subscribe" && token === "myverifytoken") {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       return res.status(200).send(challenge);
     } else {
       return res.sendStatus(403);
