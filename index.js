14cb6fdf8b5360fd905fb288c0ba9757b9912cf2
Teja Dandigunta <teja.dandigunta@example.com>
2026-05-03 15:23:53 +0530

    Simple railway test

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+// 👇 IMPORTANT: bind to 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Running on", PORT);
+});
+
 app.get("/", (req, res) => {
   res.send("OK ROOT");
 });
 
 app.get("/webhook", (req, res) => {
-  return res.send("WEBHOOK WORKING");
+  res.send("WEBHOOK OK");
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running on", PORT));
